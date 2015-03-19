958b6b972d07e3fb7395d86650677a3e5f5fa5c3
居石峻寛 <takahiro@apiserver.(none)>
2015-03-20 02:36:02 +0900

    hoge

--- walker_server/app/controllers/game_api_controller.rb
+++ walker_server/app/controllers/game_api_controller.rb
@@ -7,7 +7,7 @@ class GameApiController < ApplicationController
 			@response.push(:result => :succeed)
 			hasitem = ItemsUsers.where(:user_id => @user.id)
 			Item.all.each { |item|
-				@response.push([:id => item.id, :name => item.name, :effective_term => item.effective_term, :effective_value => item.effective_value, :description => item.description])
+				@response.push([:id => item.id, :name => item.name, :effective_term => item.effective_term, :effective_value => item.effective_value, :description => item.description, :amount => 0])
 			}
 		end
 		render :json => @response
